cc020f595255efefdab33e2c0442c763279c2df2
LorenaForero <77217186+LorenaForero@users.noreply.github.com>
2021-09-29 21:55:43 -0500

    Angular material

--- sistemaGestionRRHH/src/app/app.component.ts
+++ sistemaGestionRRHH/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sistemaGestionRRHH';
+  title = 'Sistema Gestion RRHH';
 }
