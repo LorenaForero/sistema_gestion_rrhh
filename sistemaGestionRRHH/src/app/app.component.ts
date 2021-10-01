10ba0cd0e29304a9f7d422d7f88ace4ba965733f
LorenaForero <77217186+LorenaForero@users.noreply.github.com>
2021-09-30 22:13:30 -0500

    Ajustes diseño

--- sistemaGestionRRHH/src/app/app.component.ts
+++ sistemaGestionRRHH/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sistema Gestion RRHH';
+  showFiller = true;
+
+  ngOnInit() {
+
+  }
 }
--- sistemaGestionRRHH/src/app/app.module.ts
+++ sistemaGestionRRHH/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
