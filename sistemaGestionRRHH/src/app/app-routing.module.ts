e96b9773cf7f23c800bb502b8d9c012ba47602fa
LorenaForero <77217186+LorenaForero@users.noreply.github.com>
2021-10-03 20:12:38 -0500

    rutas

--- sistemaGestionRRHH/src/app/app-routing.module.ts
+++ sistemaGestionRRHH/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CrearPeticionesComponent } from './componentes/peticiones/crear-peticiones/crear-peticiones.component';
+import { ListaPeticionesComponent } from './componentes/peticiones/lista-peticiones/lista-peticiones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'lista_peticiones', component: ListaPeticionesComponent },
+  { path: 'crear_peticiones', component: CrearPeticionesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
